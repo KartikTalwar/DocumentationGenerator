71f40991587c1372077a8d5bddd5458aa8b088aa
Kartik Talwar <hi@kartikt.com>
2013-11-07 22:51:22 +0000

    Updated docs and added example

--- example.php
+++ example.php
@@ -1,19 +1,3 @@
-## API Documentation Generator
-
-
-Given API method schema, the following class will generate a mardown documentation of the method.
-
-This is a proof of concept.
-
-
-- [Input](schema.json)
-- [Output](method.md)
-
-
-### Usage
-
-
-```php
 <?php
 
 require 'src/generator.php';
@@ -30,4 +14,3 @@ $output = $generator->compile($method_schema);
 file_put_contents('method.md', $output);
 
 ?>
-```
